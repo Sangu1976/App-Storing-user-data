0f3ee607cf0d8379a0728c90b3edc993d2d6e607
Sangeetha Joseph <sangeethajoseph76@gmail.com>
2025-04-20 20:11:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storing_user_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = " Sangu";
+            string number = "0176567899909";
+            int age = 24;
+
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+            Console.WriteLine(age);
+
+            Console.ReadLine();
+        }
+    }
+}
